bd31a859bc0d3552f80628aec077c1445aae46e1
ryzecous <r3j9c89@gmail.com>
2021-02-16 23:10:05 -0500

    changing burgers ot burger for continuity

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+const orm = require('../config/orm.js')
+
+// data
+var burger = {
+    selectAll: function(callBack) {
+        orm.selectAll("burgers", condition, function(res) {
+            callBack(res)
+        })
+    },
+    selectOne: function(condition, callBack) {
+        orm.selectOne("burgers", condition, function(res) {
+            callBack(res)
+        })
+    },
+    insertOne: function(column, columnVal, callBack) {
+        orm.insertOne(column, columnVal, function(res) {
+            callBack(res)
+        })
+    },
+    updateOne: function(column, columnVal, condition, callBack) {
+        orm.updateOne(column, columnVal, condition, function(res) {
+            callBack(res)
+        })
+    }
+}
+
+module.exports = burger
