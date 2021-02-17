ec4e9a736fd8d6b3acb823a230307b1466a83f5a
ryzecous <r3j9c89@gmail.com>
2021-02-17 02:15:12 -0500

    console.log

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,6 +9,7 @@ const burger = require("../models/burger.js");
 // Router Get
 router.get("/", function(req, res){
     burger.selectAll(function(data) {
+        // console.log(data)
         res.render("index", {burgers:data});
     })
 });
@@ -16,6 +17,7 @@ router.get("/", function(req, res){
 // Router Post
 router.post("/", function(req, res) {
     burger.insertOne("burger_name", req.body.burger_name, function(data) {
+        // console.log(data)
         res.status(200).send
     })
 })
@@ -26,6 +28,8 @@ router.put("/:id", function(req, res) {
     burger.selectOne({id: id}, function(data) {
         const selectedBurger = data[0]
 
+        // console.log(data[0])
+
         if (selectedBurger === 0) {
             selectedBurger.devoured = 1
         } else {
@@ -33,6 +37,8 @@ router.put("/:id", function(req, res) {
         }
 
         burger.updateOne("devoured", selectedBurger.devoured, {id:id}, function(data) {
+            
+        // console.log(data)
             res.status(200).end()
         })
     })
